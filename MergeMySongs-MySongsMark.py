bd5a1b05e5734673169187318276f49eaf8c721f
SergiPerez73 <sergiperez730@gmail.com>
2024-07-30 13:50:03 +0200

    Solved bug in mergin datasets

--- MergeMySongs-MySongsMark.py
+++ MergeMySongs-MySongsMark.py
@@ -4,7 +4,7 @@ df_songs = pd.read_csv('Songs.csv')
 df_songs = df_songs.drop(['name','Unnamed: 0'],axis=1)
 
 df_songsMark = pd.read_csv('SongsMark.csv')
-df_songsMark = df_songsMark.drop(['name','Unnamed: 0'],axis=1)
+df_songsMark = df_songsMark.drop(['Unnamed: 0'],axis=1)
 
 df_merged = df_songs.merge(df_songsMark, how='inner', on='id')
 df_merged = df_merged.drop(['id'],axis=1)
