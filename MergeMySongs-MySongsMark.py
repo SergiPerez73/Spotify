dddf7fa7b0bda06403f6438a7c7b03025b1bf3ad
SergiPerez73 <sergiperez730@gmail.com>
2024-07-30 12:52:00 +0200

    Merge datasets and obtain cross data

--- MergeMySongs-MySongsMark.py
+++ MergeMySongs-MySongsMark.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
 df_songs = pd.read_csv('Songs.csv')
+df_songs = df_songs.drop(['name','Unnamed: 0'],axis=1)
 
 df_songsMark = pd.read_csv('SongsMark.csv')
+df_songsMark = df_songsMark.drop(['name','Unnamed: 0'],axis=1)
 
 df_merged = df_songs.merge(df_songsMark, how='inner', on='id')
+df_merged = df_merged.drop(['id'],axis=1)
+
+df_merged.to_csv('SongsDataset.csv')
