fix(neutral-network): Apply standard scaling to all features to avoid getting stuck in local minimum

PreprocessDataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from transformers import BertTokenizer, BertModel
-from sklearn.manifold import TSNE
+import umap
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained('bert-base-uncased')
@@ -17,13 +17,15 @@ def addEmbeddingsTodf(df,col):
 
     for i in range(1,len(words)):
         outputs = np.append(outputs,(getEmbedding(words[i])),axis=0)
-        print(i)
-    tsne = TSNE(n_components=3, random_state=44,perplexity=4)
-    embeddings_2d = tsne.fit_transform(outputs)
+    
+    umap_model = umap.UMAP(n_components=3, random_state=42)
+    embeddings_2d = umap_model.fit_transform(outputs)
 
     emb_df = pd.DataFrame(embeddings_2d, columns=[col+'1', col+'2', col+'3'])
     df = pd.concat([df, emb_df], axis=1)
     
+    print('Finished ',col)
+
     return df
 
 def run(df,name):

PreprocessOneSong.py
@@ -2,6 +2,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import PreprocessDataset
+import numpy as np
+from transformers import BertTokenizer, BertModel
+import umap
 
 
 def createSpotifyApiSession(token, client_id,client_secret):
@@ -40,12 +43,60 @@ def getSongsFeatures(sp,track_ids):
     df_SongsFeatures.to_csv('OneSong.csv')
     return df_SongsFeatures
 
+def addEmbeddingsTodf(df,df_OneSong,col):
+    words = df.loc[:,col]
+    outputs = (PreprocessDataset.getEmbedding(words[0]))
+    for i in range(1,len(words)):
+        outputs = np.append(outputs,(PreprocessDataset.getEmbedding(words[i])),axis=0)
+        #print(i)
+    
+    words = df_OneSong.loc[:,col]
+    outputs_OneSong = (PreprocessDataset.getEmbedding(words[0]))
+    for i in range(1,len(words)):
+        outputs_OneSong = np.append(outputs_OneSong,(PreprocessDataset.getEmbedding(words[i])),axis=0)
+
+    umap_model = umap.UMAP(n_components=3, random_state=42)
+    embeddings_2d = umap_model.fit_transform(outputs)
+
+    embeddings_2d_OneSong = umap_model.transform(outputs_OneSong)
+
+
+    emb_df_OneSong = pd.DataFrame(embeddings_2d_OneSong, columns=[col+'1', col+'2', col+'3'])
+    df_OneSong = pd.concat([df_OneSong, emb_df_OneSong], axis=1)
+    
+    print('Finished ',col)
+
+    return df_OneSong
+
 if __name__ == "__main__":
-    sp = createSpotifyApiSession("BQD34URmIFXq6r6mKPNO00ECYyhcTktEGqRcZ1IZIkOYsChpo5lPqqtZJuwgzOSrLpPA8mJO8pEUCqyuj5TnDEfbLm9zs4rlsSHd9fPnb9B9-4owW0oICskIDbI8rkEs5vaa0XBooxN4FlCrHOJs7o-YFZ6LwCn7PurFY8FGGeeNV6pHCPrjpB6JakfivlfrZUOigcw9l5o8APUr6RU"
+    sp = createSpotifyApiSession("BQAkWRmETTwKhvP4IM-YjUaySowh-J3b0WnM6-eHyW_GwI9lnNc8IWmqc2nge3c5dc43mVRGvmWz9k5HeqKN0EO8yNIisVRl3t42-SQEEmy7AU3Kfnf8nh760Zw4K55LRurG-wNb-hag0Nw2Uyr0YmzqhuwfLtZ2thYJOmO_WTypbHiKbjn2x0lswgYLkruEEgJ6_jMEV7J9ZRmkfyA"
                                  ,'16c7c999caa041fe96aa9e01c2abf86d','4e10f9170b4444f2889bab3497806147')
 
     track_ids = ['7u0Mn9qAgZxcSWm0db2PaG','3hfmh1XIlJp2Uis4kWboqJ','0DIcssPpatAMqFXLZCxZMN','4n93SK7dQsQVu9BM5QzvAx','0nky5lP13IyrlQEGEzGQZt']
     df_OneSong = getSongsFeatures(sp,track_ids)
 
-    #Preprocess
-    PreprocessDataset.run(df_OneSong,'OneSongPreprocessed.csv')
+    df = pd.read_csv('SongsDataset.csv')
+
+    #Preprocess explicit
+    type_map = {False: 0.0, True: 1.0}
+    df_OneSong['explicit'] = df_OneSong["explicit"].map(type_map)
+
+    #Preprocess album_type
+    type_map = {'single': 0.0, 'album': 1.0, 'compilation': 1.0}
+    df_OneSong['album_type'] = df_OneSong["album_type"].map(type_map)
+
+    #Preprocess album_name
+    df_OneSong = addEmbeddingsTodf(df,df_OneSong,'album_name')
+
+    #Preprocess release_date
+    df_OneSong = addEmbeddingsTodf(df,df_OneSong,'release_date')
+
+    #Preprocess artist_name
+    df_OneSong = addEmbeddingsTodf(df,df_OneSong,'artist_name')
+
+    #Preprocess name
+    df_OneSong = addEmbeddingsTodf(df,df_OneSong,'name')
+
+    df_OneSong = df_OneSong.drop(['album_name','release_date','artist_name','name'],axis=1)
+
+    df_OneSong.to_csv('OneSongPreprocessed.csv')

logistic_spotify_recom.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 Data = pd.read_csv('PreprocessedDataset.csv')
 
@@ -18,19 +19,25 @@ y = y.values
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.1, random_state=1234)
 
-X_train = torch.from_numpy(np.array(X_train).astype(np.float32))
-X_valid = torch.from_numpy(np.array(X_valid).astype(np.float32))
-X_test = torch.from_numpy(np.array(X_test).astype(np.float32))
+sc = StandardScaler()
+
+X_train = sc.fit_transform(X_train) #to fit it in mean 0 and unit variance
+X_test = sc.transform(X_test) #uses the same mean and variance previously calculated not to disturb the results
+X_valid = sc.transform(X_valid)
+
+X_train = torch.from_numpy(X_train.astype(np.float32))
+X_valid = torch.from_numpy(X_valid.astype(np.float32))
+X_test = torch.from_numpy(X_test.astype(np.float32))
 
 y_train = torch.from_numpy(y_train.astype(np.float32))
 y_test = torch.from_numpy(y_test.astype(np.float32))
 
 n_features = X_train.shape[1]
 
+
+
 y_train = y_train.view(y_train.shape[0],1) #multiple rows, only 1 column
 y_test = y_test.view(y_test.shape[0],1)
-
-print(X_train)
 
 # 1) model
 
@@ -39,17 +46,13 @@ print(X_train)
 class LogisticRegression(nn.Module):
     def __init__(self,n_input_features):
         super(LogisticRegression,self).__init__()
-
-        self.l1 = nn.Linear(n_input_features, 16)
-        self.l2 = nn.Linear(16, 16)
-        self.l3 = nn.Linear(16, 1)
+        self.linear = nn.Linear(n_input_features,3) #number of features and only one output that is a probability
+        self.linear2 = nn.Linear(3,1)
 
     def forward(self,x):
-        y_predicted1 = torch.sigmoid(self.l1(x))
-        y_predicted2 = torch.sigmoid(self.l2(y_predicted1))
-        y_predicted3 = torch.sigmoid(self.l3(y_predicted2))
-
-        return torch.sigmoid(y_predicted3)
+        y_predicted1 = torch.sigmoid(self.linear(x))
+        y_predicted2 = torch.sigmoid(self.linear2(y_predicted1))
+        return y_predicted2
 
 model = LogisticRegression(n_features)
 
@@ -61,7 +64,7 @@ optimizer = torch.optim.SGD(model.parameters(),lr = learning_rate)
 
 # 3) Train loop
 
-n_iterations = 2000
+n_iterations = 200000
 
 X_accuracy = []
 Y_accuracy = []
@@ -69,8 +72,7 @@ Y_accuracy = []
 def test_accuracy():
     with torch.no_grad():
         y_predicted = model(X_test)
-        y_predictedcls = y_predicted.round() #As the y it's a probability, we can round it. In this case, as our model has
-        # multiple samples, the y_predicted is a tensor with a multiple rows and in each one has its y result.
+        y_predictedcls = y_predicted.round()
 
         y_testcls = y_test.round()
 
@@ -80,18 +82,18 @@ def test_accuracy():
 
 for epoch in range(n_iterations):
     y_pred = model.forward(X_train)
-    df = pd.DataFrame(y_pred.detach().numpy(), columns=['columna'])
     loss = criterion(y_pred,y_train)
     loss.backward()
     optimizer.step()
     optimizer.zero_grad()
-    
+
     if epoch % 2 == 0:
         X_accuracy.append(epoch)
-        Y_accuracy.append(test_accuracy().item())
+        Y_accuracy.append(loss.item())
     
-    if epoch % (500) == 0 or epoch==n_iterations:
-        print(f'loss = {loss:.8f}')
+    if (epoch+1) % (500) == 0 or epoch==n_iterations:
+        [w,b,w,b] = model.parameters()
+        print(f'epoch {epoch+1}: w = {w[0][0].item():.3f}, b = {b[0].item():.3f}, ,loss = {loss:.8f}')
 
 X_accuracy = np.array(X_accuracy)
 Y_accuracy = np.array(Y_accuracy)
@@ -109,6 +111,9 @@ with torch.no_grad():
     print(f'accuracy = {acc:.4f}')
 
 with torch.no_grad():
-    y_predicted = model(X_valid)
+    y_valid = model(X_valid)
 
-    print('y_valid',y_predicted)
+    print('y_valid',y_valid)
+
+
+model_parameters = model.state_dict()