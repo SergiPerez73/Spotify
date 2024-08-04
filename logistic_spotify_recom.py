d3625b3547f525f4578b565e3087edcdef0f81c6
SergiPerez73 <sergiperez730@gmail.com>
2024-08-04 18:26:03 +0200

    feat(neural-network): organize code in a class with different functions
    Create SpotifySongsRecommendationModel class with functions to prepare_Data, trainLoop, showResults and get accuracy and loss for test

--- logistic_spotify_recom.py
+++ logistic_spotify_recom.py
@@ -6,43 +6,6 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-Data = pd.read_csv('PreprocessedDataset.csv')
-
-Data_valid = pd.read_csv('OneSongPreprocessed.csv') 
-
-X = Data.drop(['mark','Unnamed: 0'],axis=1)
-X_valid = Data_valid.drop(['id','Unnamed: 0'],axis=1)
-
-y = Data['mark']
-
-y = y.values
-
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.1, random_state=1234)
-
-sc = StandardScaler()
-
-X_train = sc.fit_transform(X_train) #to fit it in mean 0 and unit variance
-X_test = sc.transform(X_test) #uses the same mean and variance previously calculated not to disturb the results
-X_valid = sc.transform(X_valid)
-
-X_train = torch.from_numpy(X_train.astype(np.float32))
-X_valid = torch.from_numpy(X_valid.astype(np.float32))
-X_test = torch.from_numpy(X_test.astype(np.float32))
-
-y_train = torch.from_numpy(y_train.astype(np.float32))
-y_test = torch.from_numpy(y_test.astype(np.float32))
-
-n_features = X_train.shape[1]
-
-
-
-y_train = y_train.view(y_train.shape[0],1) #multiple rows, only 1 column
-y_test = y_test.view(y_test.shape[0],1)
-
-# 1) model
-
-# f = wx + b, sigmoid at the end
-
 class LogisticRegression(nn.Module):
     def __init__(self,n_input_features):
         super(LogisticRegression,self).__init__()
@@ -54,66 +17,114 @@ class LogisticRegression(nn.Module):
         y_predicted2 = torch.sigmoid(self.linear2(y_predicted1))
         return y_predicted2
 
-model = LogisticRegression(n_features)
+class SpotifySongsRecommendationModel:
 
-# 2) Loss and optimizer
-criterion  = nn.MSELoss()
+    def prepareData(self,test_size):
+        Data = pd.read_csv('PreprocessedDataset.csv')
 
-learning_rate = 0.1
-optimizer = torch.optim.SGD(model.parameters(),lr = learning_rate)
+        Data_valid = pd.read_csv('OneSongPreprocessed.csv') 
 
-# 3) Train loop
+        X = Data.drop(['mark','Unnamed: 0'],axis=1)
+        X_valid = Data_valid.drop(['id','Unnamed: 0'],axis=1)
 
-n_iterations = 200000
+        y = Data['mark']
 
-X_accuracy = []
-Y_accuracy = []
+        y = y.values
 
-def test_accuracy():
-    with torch.no_grad():
-        y_predicted = model(X_test)
-        y_predictedcls = y_predicted.round()
+        X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=test_size, random_state=1234)
 
-        y_testcls = y_test.round()
+        sc = StandardScaler()
 
-        acc = y_predictedcls.eq(y_testcls).sum() / y_test.shape[0]
+        X_train = sc.fit_transform(X_train) #to fit it in mean 0 and unit variance
+        X_test = sc.transform(X_test) #uses the same mean and variance previously calculated not to disturb the results
+        X_valid = sc.transform(X_valid)
 
-        return acc
+        self.X_train = torch.from_numpy(X_train.astype(np.float32))
+        self.X_valid = torch.from_numpy(X_valid.astype(np.float32))
+        self.X_test = torch.from_numpy(X_test.astype(np.float32))
 
-for epoch in range(n_iterations):
-    y_pred = model.forward(X_train)
-    loss = criterion(y_pred,y_train)
-    loss.backward()
-    optimizer.step()
-    optimizer.zero_grad()
+        y_train = torch.from_numpy(y_train.astype(np.float32))
+        y_test = torch.from_numpy(y_test.astype(np.float32))
 
-    if epoch % 2 == 0:
-        X_accuracy.append(epoch)
-        Y_accuracy.append(loss.item())
-    
-    if (epoch+1) % (500) == 0 or epoch==n_iterations:
-        [w,b,w,b] = model.parameters()
-        print(f'epoch {epoch+1}: w = {w[0][0].item():.3f}, b = {b[0].item():.3f}, ,loss = {loss:.8f}')
+        self.y_train = y_train.view(y_train.shape[0],1) #multiple rows, only 1 column
+        self.y_test = y_test.view(y_test.shape[0],1)
 
-X_accuracy = np.array(X_accuracy)
-Y_accuracy = np.array(Y_accuracy)
-plt.plot(X_accuracy,Y_accuracy)
-plt.show()
+    def trainLoop(self,lr,n_iterations,print_freq,test_freq,model_path):
 
-with torch.no_grad():
-    y_predicted = model(X_test)
-    y_predictedcls = y_predicted.round() #As the y it's a probability, we can round it. In this case, as our model has
-    # multiple samples, the y_predicted is a tensor with a multiple rows and in each one has its y result.
+        n_features = self.X_train.shape[1]
+        self.model = LogisticRegression(n_features)
 
-    y_testcls = y_test.round()
-    acc = y_predictedcls.eq(y_testcls).sum() / y_test.shape[0]
+        if model_path != '':
+            self.model.load_state_dict(torch.load(model_path))
 
-    print(f'accuracy = {acc:.4f}')
+        self.criterion  = nn.MSELoss()
 
-with torch.no_grad():
-    y_valid = model(X_valid)
+        optimizer = torch.optim.SGD(self.model.parameters(),lr = lr)
 
-    print('y_valid',y_valid)
+        self.x_axis = []
+        self.accuracy_test = []
+        self.loss_train = []
+        self.loss_test = []
+
+        
+        for epoch in range(n_iterations):
+            y_pred = self.model.forward(self.X_train)
+            loss = self.criterion(y_pred,self.y_train)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if epoch % test_freq == 0 or epoch==n_iterations-1:
+                self.x_axis.append(epoch)
+                self.loss_train.append(loss.item())
+                self.accuracy_test.append(self.test_accuracy().item())
+                self.loss_test.append(self.test_loss().item())
+            
+            if epoch % print_freq == 0 or epoch==n_iterations-1:
+                [w,b,w,b] = self.model.parameters()
+                print(f'epoch {epoch}: loss = {loss:.8f}')
 
 
-model_parameters = model.state_dict()
+    def showResults(self):
+        plt.plot(self.x_axis,self.loss_train)
+        plt.show()
+
+        plt.plot(self.x_axis,self.accuracy_test)
+        plt.show()
+
+        plt.plot(self.x_axis,self.loss_test)
+        plt.show()
+
+        with torch.no_grad():
+            y_valid = self.model(self.X_valid)
+
+            print('y_valid',y_valid)
+
+    def test_accuracy(self):
+        with torch.no_grad():
+            y_predicted = self.model(self.X_test)
+            y_predictedcls = y_predicted.round()
+
+            y_testcls = self.y_test.round()
+
+            acc = y_predictedcls.eq(y_testcls).sum() / self.y_test.shape[0]
+
+            return acc
+
+    def test_loss(self):
+        with torch.no_grad():
+            y_predicted = self.model(self.X_test)
+
+            loss = self.criterion(y_predicted,self.y_test)
+
+            return loss
+
+
+if __name__ == "__main__":
+    SSRM = SpotifySongsRecommendationModel()
+    SSRM.prepareData(0.1)
+    SSRM.trainLoop(0.1,20000,1000,1000,'model.pt')
+    SSRM.showResults()
+
+    model_parameters = SSRM.model.state_dict()
+    torch.save(model_parameters, 'model.pt')
